1e1c6036a047ae4539ab85a1df929bece5b19b2f
Gil Shaashua <gilshaashua@gmail.com>
2023-10-24 19:22:03 +0300

    added option to follow a post creator from post-card component

--- src/app/cmps/post-header/post-header.component.ts
+++ src/app/cmps/post-header/post-header.component.ts
@@ -1,14 +1,44 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { User } from '../../models/user.model';
 import { Post } from '../../models/post.model';
+import { firstValueFrom } from 'rxjs';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
     selector: 'post-header',
     templateUrl: './post-header.component.html',
     styleUrls: ['./post-header.component.scss'],
 })
-export class PostHeaderComponent {
+export class PostHeaderComponent implements OnInit {
+    constructor(private authService: AuthService) {}
     @Input() creator!: User;
     @Input() post!: Post;
     @Input() isComponentInitialized!: boolean;
+    @Output() onToggleFollow = new EventEmitter();
+
+    isMoreOptionsModalShown = false;
+    isFollowClicked = false;
+    loggedInUser!: any;
+
+    async ngOnInit() {
+        const userFromDB$ = this.authService.getUserById(this.creator._id);
+        const userFromDB = await firstValueFrom(userFromDB$);
+
+        const loggedInUser = this.authService.getLoggedInUser();
+        this.loggedInUser = loggedInUser;
+
+        this.isFollowClicked = !!(userFromDB as User).followedByUsers.find(
+            (followedByUser: User | string) =>
+                followedByUser === loggedInUser!.user!.uid,
+        );
+    }
+
+    toggleFollow() {
+        this.isFollowClicked = !this.isFollowClicked;
+        this.onToggleFollow.emit({
+            post: this.post,
+            user: this.creator,
+            isFollowClicked: this.isFollowClicked,
+        });
+    }
 }
